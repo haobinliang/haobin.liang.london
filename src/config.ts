a4dc7f86c1a4094a076170bc6357e2575d745e53
Haobin Liang <haobin.liang@outlook.com>
2024-08-03 19:21:52 +0100

    Updated url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://haobin.liang.london/", // replace this with your deployed domain
   author: "Haobin Liang",
   desc: "This is a blog about change, life, and sometimes work.",
   title: "Haobin Liang",
